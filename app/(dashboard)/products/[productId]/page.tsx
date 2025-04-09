7ed2b9e7b5ab6eecc38e3bc17ad8c6ccc08f431a
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2025-04-09 14:44:31 +0700

    fix product id and fix dependency for images cloudinary

--- app/(dashboard)/products/[productId]/page.tsx
+++ app/(dashboard)/products/[productId]/page.tsx
@@ -1,33 +1,30 @@
-"use client"
+"use client";
 
-import Loader from '@/components/custom ui/Loader'
-import ProductForm from '@/components/products/ProductForm'
-import React, { useEffect, useState } from 'react'
+import Loader from "@/components/custom ui/Loader";
+import ProductForm from "@/components/products/ProductForm";
+import React, { useEffect, useState } from "react";
 
-const ProductDetails = ({ params }: { params: { productId: string }}) => {
-  const [loading, setLoading] = useState(true)
-  const [productDetails, setProductDetails] = useState<ProductType | null>(null)
-
-  const getProductDetails = async () => {
-    try { 
-      const res = await fetch(`/api/products/${params.productId}`, {
-        method: "GET"
-      })
-      const data = await res.json()
-      setProductDetails(data)
-      setLoading(false)
-    } catch (err) {
-      console.log("[productId_GET]", err)
-    }
-  }
+const ProductDetails = ({ params }: { params: { productId: string } }) => {
+  const [loading, setLoading] = useState(true);
+  const [productDetails, setProductDetails] = useState<ProductType | null>(null);
 
   useEffect(() => {
-    getProductDetails()
-  }, [])
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`/api/products/${params.productId}`, {
+          method: "GET",
+        });
+        const data = await res.json();
+        setProductDetails(data);
+        setLoading(false);
+      } catch (err) {
+        console.log("[productId_GET]", err);
+      }
+    };
+    fetchData();
+  }, [params.productId]); // Thêm params.productId làm dependency
 
-  return loading ? <Loader /> : (
-    <ProductForm initialData={productDetails} />
-  )
-}
+  return loading ? <Loader /> : <ProductForm initialData={productDetails} />;
+};
 
-export default ProductDetails
+export default ProductDetails;
